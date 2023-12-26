40bf7dd5cd4db6870a73e7ce9848bd530119da79
Shreyash Nalawade <shreyashnalawade11@gmail.com>
2023-12-26 10:03:18 +0530

    Added functons

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,10 +1,16 @@
 const { readSearchTerms } = require("../../utils/fileProcessing");
 const { main } = require("../../utils/pageProcessing");
 
-(async () => {
-  const searchTerms = readSearchTerms();
-  console.log(searchTerms);
-  for (const searchTerm of searchTerms) {
-    await main(searchTerm);
-  }
-})();
+exports.handler = async () => {
+  // (async () => {
+  //   const searchTerms = readSearchTerms();
+  //   console.log(searchTerms);
+  //   for (const searchTerm of searchTerms) {
+  //     await main(searchTerm);
+  //   }
+  // })();
+  return {
+    statusCode: 200,
+    body: "Hello, World!",
+  };
+};
