4fe8c20be689c1673d69084b857a699dcb0ef586
Shreyash Nalawade <shreyashnalawade11@gmail.com>
2023-12-26 10:17:25 +0530

    Added functons

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,6 +1,7 @@
 const { readSearchTerms } = require("../../utils/fileProcessing");
 const { main } = require("../../utils/pageProcessing");
-
+const puppeteer = require("puppeteer");
+const path = require("path");
 exports.handler = async () => {
   // (async () => {
   //   const searchTerms = readSearchTerms();
@@ -9,9 +10,26 @@ exports.handler = async () => {
   //     await main(searchTerm);
   //   }
   // })();
-  console.log("requrest recived");
+  const exce = path.join(
+    __dirname,
+    "../../.cache/puppeteer/chrome/win64-119.0.6045.105/chrome-win64/chrome.exe"
+  );
+  const userData = path.join(__dirname, "../../temp");
+  const browser = await puppeteer.launch({
+    headless: "new",
+    executablePath: exce,
+    userDataDir: userData,
+  });
+
+  const page = await browser.newPage();
+  await page.goto(
+    "https://en.wikipedia.org/wiki/Cron#:~:text=Asterisks%20(also%20known%20as%20wildcard,second%20when%20seconds%20are%20supported."
+  );
+  const title = await page.evaluate(() => {
+    return document.querySelector(`#firstHeading`).textContent;
+  });
   return {
     statusCode: 200,
-    body: "Hello, World!",
+    body: title,
   };
 };
